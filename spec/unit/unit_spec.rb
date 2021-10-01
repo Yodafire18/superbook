92c6cbe1c6b820deb561a742fec8d86a589c5552
Yodafire18 <anu.srini21@gmail.com>
2021-09-30 22:01:23 -0500

    Final with date fix

--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -3,16 +3,31 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   subject do
-    described_class.new(title: 'harry potter')
+    described_class.new(title: 'hermione granger', author: 'rowling', published_date: '01/01/2001', price: '1000')
   end
 
-  it 'is valid title' do
-    described_class.new(title: 'harry potter' author:'rowling' price:1000 published_date:'01/01/2001')  
-    expect(subject).to be_valid
+
+  it 'is valid title with valid attributes' do
+
+    title = Book.new(title: 'hermione granger', author: 'rowling', published_date: '01/01/2001', price: '1000')
+    expect(title).to be_valid
   end
 
-  it 'is not valid without a name' do
-    subject.title = nil
-    expect(subject).not_to be_valid
+
+  it 'is valid author with valid attributes' do
+
+    author = Book.new(title: 'hermione granger', author: 'rowling', published_date: '01/01/2001', price: '1000')
+    expect(author).to be_valid
+  end
+
+
+  it 'is valid date with valid attributes' do
+    published_date = Book.new(title: 'hermione granger', author: 'rowling', published_date: '01/01/2001', price: '1000')
+    expect(published_date).to be_valid
+  end
+
+  it 'is valid price with valid attributes' do
+    price = Book.new(title: 'hermione granger', author: 'rowling', published_date: '01/01/2001', price: '1000')
+    expect(price).to be_valid
   end
 end
